28c09a9619cf6066ed14de8e89ebbea77de63bb2
darky <growlbox123g@gmail.com>
2022-02-27 13:19:06 +0100

    svg update

--- pages/ComponentProduct/ProductPage.tsx
+++ pages/ComponentProduct/ProductPage.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import * as React from "react";
 import Cropper from "react-cropper";
 import "cropperjs/dist/cropper.css";
 import "./Demo.module.css";
@@ -6,7 +7,7 @@ import "./Demo.module.css";
 const defaultSrc =
     "/donotopen.png";
 
-export const Demo: React.FC = () => {
+export const Demo: React.FC = (props) => {
     const [image, setImage] = useState(defaultSrc);
     const [cropData, setCropData] = useState("#");
     const [cropper, setCropper] = useState<any>();
@@ -27,13 +28,11 @@ export const Demo: React.FC = () => {
 
     const getCropData = () => {
         if (typeof cropper !== "undefined") {
-            setCropData(cropper.getCroppedCanvas().toDataURL());
-            
+            setCropData(cropper.getCroppedCanvas().toDataURL());  
         }
     };
-    console.log(cropData);
     return (
-        <div>
+        <div className="bg-slate-800">
             
             <div style={{ width: "100%" }}>
                 <input type="file" onChange={onChange} />
@@ -66,17 +65,262 @@ export const Demo: React.FC = () => {
                     style={{ width: "50%", float: "right", height: "300px" }}
                 >
                     <h1>
-                        <span>Crop</span>
+                        
                         <button style={{ float: "right" }} onClick={getCropData}>
                             Crop Image
                         </button>
                     </h1>
-                    <img style={{ width: "35%" }} src={cropData} alt="cropped" />
                 </div>
                 
             </div>
             <br style={{ clear: "both" }} />
-            
+            <div className="">
+                <svg
+                    id="instagramCover"
+                    viewBox="0 0 839 1090"
+                    xmlns="http://www.w3.org/2000/svg"
+                    style={{
+                        maxHeight: "65vh",
+                        backgroundColor: "blue",
+                        maxWidth: "477.978px",
+                    }}
+                    data-v-6b966f65=""
+                    {...props}
+                >
+                    <radialGradient id="rg" r="150%" cx="30%" cy="107%">
+                        <stop stopColor="#fdf497" offset={0} />
+                        <stop stopColor="#fdf497" offset={0.05} />
+                        <stop stopColor="#fd5949" offset={0.45} />
+                        <stop stopColor="#d6249f" offset={0.6} />
+                        <stop stopColor="#285AEB" offset={0.9} />
+                    </radialGradient>
+                    <pattern
+                        id="profileImagePattern"
+                        patternUnits="userSpaceOnUse"
+                        width={47}
+                        height={47}
+                    >
+                        <image href="/donotopen.png" x={0} y={0} width={47} height={47} />
+                    </pattern>
+                    <g>
+                        <title>{"background"}</title>
+                        <rect
+                            id="canvas_background"
+                            x={-1}
+                            y={-1}
+                            width={849.47479}
+                            height={1092.63641}
+                            fill="none"
+                        />
+                        <g id="canvasGrid" display="none">
+                            <rect
+                                id="svg_2"
+                                width="100%"
+                                height="100%"
+                                x={0}
+                                y={0}
+                                strokeWidth={0}
+                                fill="url(#gridpattern)"
+                            />
+                        </g>
+                    </g>
+                    <g>
+                        <title>{"Layer 1"}</title>
+                        <rect
+                            height={575}
+                            width={575}
+                            x={132}
+                            y={206.78841}
+                            fill="var(--fill-color)"
+                        />
+                        <image
+                            id="svg_6"
+                            href={cropData}
+                            height={575}
+                            width={575}
+                            x={132}
+                            y={206.78841}
+                        />
+                        <g id="svg_30">
+                            <g id="svg_24">
+                                <ellipse
+                                    id="svg_23"
+                                    fill="url(#profileImagePattern)"
+                                    strokeWidth={0}
+                                    cx={162.62537}
+                                    cy={161.12961}
+                                    rx={24}
+                                    ry={24}
+                                />
+                            </g>
+                            <foreignObject x={200} y={150} width={400} height={50}>
+                                <div
+                                    style={{
+                                        fontFamily: "Montserrat, sans-serif",
+                                        fontSize: 21,
+                                        color: "var(--color)",
+                                        fontWeight: "bold",
+                                        textAlign: "left",
+                                    }}
+                                >
+                                    <span>{"uzivatelske_jmeno"}</span>
+                                </div>
+                            </foreignObject>
+                        </g>
+                        <ellipse
+                            id="svg_31"
+                            fill="var(--color)"
+                            stroke="var(--color)"
+                            strokeWidth={0}
+                            cx={701.68067}
+                            cy={161.25276}
+                            rx={3}
+                            ry={3}
+                        />
+                        <ellipse
+                            id="svg_32"
+                            fill="var(--color)"
+                            stroke="var(--color)"
+                            strokeWidth={0}
+                            cx={692.12201}
+                            cy={161.14713}
+                            rx={3}
+                            ry={3}
+                        />
+                        <ellipse
+                            id="svg_33"
+                            fill="var(--color)"
+                            stroke="var(--color)"
+                            strokeWidth={0}
+                            cx={682.44134}
+                            cy={161.20168}
+                            rx={3}
+                            ry={3}
+                        />
+                        <ellipse
+                            id="svg_35"
+                            fill="var(--color)"
+                            stroke="var(--color)"
+                            strokeWidth={0}
+                            cx={701.68067}
+                            cy={161.25276}
+                            rx={3}
+                            ry={3}
+                        />
+                    </g>
+                    <g>
+                        <title>{"Layer 2"}</title>
+                        <polygon
+                            id="XMLID_35_"
+                            strokeWidth={3}
+                            fill="none"
+                            points="673.1809692382812,806.3119049072266 707.2666625976562,806.3119049072266 707.2666625976562,843.6880950927734 690.5681762695312,828.1826858520508 673.1809692382812,843.6880950927734 "
+                            stroke="var(--color)"
+                            className="st0"
+                        />
+                        <g id="XMLID_8_" stroke="null">
+                            <path
+                                id="svg_1"
+                                strokeWidth={3}
+                                stroke="var(--color)"
+                                fill="none"
+                                d="m153.48936,809.85026c-5.49226,-9.16352 -20.93617,-6.48842 -20.93617,4.99397c0,7.90655 9.71962,15.99121 20.93617,26.81542c11.2183,-10.82422 20.93617,-18.90888 20.93617,-26.81542c0,-11.5214 -15.48404,-14.08963 -20.93617,-4.99397z"
+                            />
+                        </g>
+                        <g id="XMLID_30_" fillOpacity={0} stroke="null">
+                            <path
+                                id="XMLID_20_"
+                                fill="var(--color)"
+                                strokeWidth={3}
+                                d="m232.43284,840.64192c-3.00747,1.63691 -6.39087,2.54631 -9.96223,2.54631c-11.278,0 -20.3004,-8.73019 -20.3004,-19.64292s9.02239,-19.64292 20.3004,-19.64292c11.278,0 20.3004,8.73019 20.3004,19.64292c0,3.63758 -0.93984,7.09327 -2.8195,10.00334"
+                                stroke="var(--color)"
+                                className="st1"
+                            />
+                            <polyline
+                                id="XMLID_25_"
+                                fill="var(--color)"
+                                strokeWidth={3}
+                                points="232.432861328125,840.6419067382812 242.5830535888672,844.0975952148438 239.9515380859375,833.5486450195312 "
+                                stroke="var(--color)"
+                                className="st0"
+                            />
+                        </g>
+                        <g id="XMLID_32_" stroke="null">
+                            <polygon
+                                id="XMLID_31_"
+                                strokeWidth={3}
+                                fill="none"
+                                points="269,806.0849475860596 311.8564147949219,806.0849475860596 290.4281768798828,841.9150581359863 284.9771423339844,820.8171501159668 "
+                                stroke="var(--color)"
+                                className="st0"
+                            />
+                            <line
+                                id="XMLID_33_"
+                                strokeWidth={3}
+                                fill="none"
+                                y2={806.08494}
+                                x2={311.8564}
+                                y1={820.81713}
+                                x1={284.97717}
+                                stroke="var(--color)"
+                                className="st0"
+                            />
+                        </g>
+                        <text
+                            id="svg_4"
+                            xmlSpace="preserve"
+                            textAnchor="start"
+                            fontFamily="Montserrat, sans-serif"
+                            fontSize={21}
+                            y={883}
+                            x={210}
+                            strokeWidth={0}
+                            stroke="var(--color)"
+                            fill="var(--color)"
+                        >
+                            {"To se l\xEDb\xED "}
+                            <tspan
+                                style={{
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                {"___"}
+                            </tspan>
+                            {" a "}
+                            <tspan
+                                style={{
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                {"dal\u0161\xEDm (___)"}
+                            </tspan>
+                        </text>
+                        <foreignObject x={132} y={900} width={575} height={120}>
+                            <div
+                                style={{
+                                    fontFamily: "Montserrat, sans-serif",
+                                    fontSize: 21,
+                                    color: "var(--color)",
+                                    textAlign: "left",
+                                }}
+                            >
+                                <b>{"uzivatelske_jmeno"}</b>
+                                <span>{"Popisek"}</span>
+                            </div>
+                        </foreignObject>
+                        <image
+                            id="svg_6"
+                            
+                            height={50}
+                            width={50}
+                            y={847.5}
+                            x={132.5}
+                        />
+                    </g>
+                </svg>
+
+
+            </div>
         </div>
     );
 };
