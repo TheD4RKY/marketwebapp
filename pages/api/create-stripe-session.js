2f086492ce2146d5c6b58c0aba422de941545121
darky <growlbox123g@gmail.com>
2022-04-09 14:31:58 +0200

    stripesesh

---
+++ pages/api/create-stripe-session.js
@@ -0,0 +1,12 @@
+const products = [
+    
+    {
+        id: 'price_1KjVHsG1sCeub6SXWTAYPOC1',
+        name: 'Dřevo',
+        price: 200,
+        currency: 'CZK',
+        image: '/donotopen.jpg',
+    },
+];
+
+export default products;
